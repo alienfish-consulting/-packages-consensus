3a5a3297fc9b0b2ab7584d44ddf2720a2349f19a
TerryLeeAllenJr <lee.allen.sc@gmail.com>
2023-05-31 22:48:53 -0400

    Added husky, fixed nyc, and updated deps

--- src/boilerplate.ts
+++ src/boilerplate.ts
@@ -1,5 +1,18 @@
-export const createHeader = () => {
-    const header = document.createElement('h2');
-    header.innerText = 'Lee\'s Package Building Boiler Plate';
-    return header;
-}
+/**
+ * @module boilerplate.ts
+ */
+/**
+ * fn: createHeader
+ *
+ * @remarks Ha ha ha
+ * @param text
+ */
+export const createHeader = (text?: string) => {
+  const header = document.createElement('h2');
+  header.innerText = text || 'Lee\'s Package Building Boiler Plate';
+  return header;
+};
+
+export const foo = <T>(params: T): T | boolean => {
+  return (1+1 == 2) || params;
+};
--- test/module-compatibility/commonjs.js
+++ test/module-compatibility/commonjs.js
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const createHeader = require('boilerplage').createHeader;
+const createHeader = require('boilerplate').createHeader;
 console.log(require('boilerplate'));
 console.log(createHeader());
